a47ab51e3115602bc4c43142f26f0b9a9c678037
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2026-06-04 15:54:56 +0100

    UPDATED

--- src/components/Interactive3DBackground.tsx
+++ src/components/Interactive3DBackground.tsx
@@ -1,5 +1,5 @@
 export default function Interactive3DBackground() {
   return (
-    <div className="fixed inset-0 w-full h-full pointer-events-none -z-10 bg-[#070708]" />
+    <div className="" />
   );
 }
