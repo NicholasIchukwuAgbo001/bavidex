0e5abbdca77029dc61d17d430be92094fdc9277b
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2026-06-04 15:35:57 +0100

    UPDATED

--- src/components/ShopPage.tsx
+++ src/components/ShopPage.tsx
@@ -121,7 +121,7 @@ export default function ShopPage({
             </button>
           </div>
         ) : (
-          <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {filteredProducts.filter(Boolean).map((p) => (
               <ProductCard
                 key={p.id}
